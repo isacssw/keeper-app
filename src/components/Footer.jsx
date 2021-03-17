f004043e206ca7bad514d3daea947c7860067bfb
Isac X <isacssw@gmail.com>
2021-03-17 11:17:00 +0000

    adding components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+    
+    return (
+        <p>Copyright {year}</p>
+    )
+}
+
+export default Footer
