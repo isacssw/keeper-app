5ac3050cc161b2b2711d3d005584a4591f44e1bd
Isac X <isacssw@gmail.com>
2021-03-17 11:53:32 +0000

    adding styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Footer() {
     const date = new Date();
     const year = date.getFullYear();
-    
+
     return (
-        <p>Copyright {year}</p>
+        <footer>
+            <p>Copyright © {year}</p>
+        </footer>
     )
 }
 
